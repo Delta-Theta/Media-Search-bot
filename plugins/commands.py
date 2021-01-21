1f8240bc904e308c52ba55508521bfd874e911fa
Delta-Theta <77450982+Delta-Theta@users.noreply.github.com>
2021-01-21 16:00:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -15,7 +15,7 @@ async def start(bot, message):
         user = message.from_user
         await message.reply(INVITE_MSG.format(username=user.username, first_name=user.first_name, last_name=user.last_name))
     else:
-         buttons = [[
+        buttons = [[
             InlineKeyboardButton('Click To Search 👆', switch_inline_query_current_chat=''), 
   
         ]]
