8461399b2cd66a8d24876157789c7dcb9fe50d6e
Delta-Theta <77450982+Delta-Theta@users.noreply.github.com>
2021-07-10 18:24:54 +0000

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -18,7 +18,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='plz click here\nyou are not subscribe our channel',
+                           switch_pm_text='plz click here you are not subscribe our channel',
                            switch_pm_parameter="subscribe")
         return
 
